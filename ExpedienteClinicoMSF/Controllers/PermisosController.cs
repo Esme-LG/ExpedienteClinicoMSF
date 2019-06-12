fc72bb0daa75bf525822e45e11b9b8b7909992cc
Esmeralda López <lg.esme@hotmail.com>
2019-06-11 23:07:53 -0600

    Agregar controlador de permisos

---
+++ ExpedienteClinicoMSF/Controllers/PermisosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpedienteClinicoMSF.Models;
+
+namespace ExpedienteClinicoMSF.Controllers
+{
+    public class PermisosController : Controller
+    {
+        private readonly expedienteContext _context;
+
+        public PermisosController(expedienteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Permisos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Permisos.ToListAsync());
+        }
+
+        // GET: Permisos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permisos = await _context.Permisos
+                .FirstOrDefaultAsync(m => m.PermisoId == id);
+            if (permisos == null)
+            {
+                return NotFound();
+            }
+
+            return View(permisos);
+        }
+
+        // GET: Permisos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Permisos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PermisoId,Permiso,EstadoPermiso")] Permisos permisos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(permisos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(permisos);
+        }
+
+        // GET: Permisos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permisos = await _context.Permisos.FindAsync(id);
+            if (permisos == null)
+            {
+                return NotFound();
+            }
+            return View(permisos);
+        }
+
+        // POST: Permisos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PermisoId,Permiso,EstadoPermiso")] Permisos permisos)
+        {
+            if (id != permisos.PermisoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(permisos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PermisosExists(permisos.PermisoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(permisos);
+        }
+
+        // GET: Permisos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permisos = await _context.Permisos
+                .FirstOrDefaultAsync(m => m.PermisoId == id);
+            if (permisos == null)
+            {
+                return NotFound();
+            }
+
+            return View(permisos);
+        }
+
+        // POST: Permisos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var permisos = await _context.Permisos.FindAsync(id);
+            _context.Permisos.Remove(permisos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PermisosExists(int id)
+        {
+            return _context.Permisos.Any(e => e.PermisoId == id);
+        }
+    }
+}
