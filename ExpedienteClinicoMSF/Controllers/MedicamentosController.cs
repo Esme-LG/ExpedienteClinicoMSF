29ec6f076ad95ab74dfaf9cb2bbb07ad58b026ac
Esmeralda López <lg.esme@hotmail.com>
2019-06-13 01:09:19 -0600

    Agregado controlador de medicamentos

---
+++ ExpedienteClinicoMSF/Controllers/MedicamentosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpedienteClinicoMSF.Models;
+
+namespace ExpedienteClinicoMSF.Controllers
+{
+    public class MedicamentosController : Controller
+    {
+        private readonly expedienteContext _context;
+
+        public MedicamentosController(expedienteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Medicamentos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Medicamentos.ToListAsync());
+        }
+
+        // GET: Medicamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicamentos = await _context.Medicamentos
+                .FirstOrDefaultAsync(m => m.MedicamentosId == id);
+            if (medicamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicamentos);
+        }
+
+        // GET: Medicamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medicamentos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MedicamentosId,Medicamento,Stock")] Medicamentos medicamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(medicamentos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(medicamentos);
+        }
+
+        // GET: Medicamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicamentos = await _context.Medicamentos.FindAsync(id);
+            if (medicamentos == null)
+            {
+                return NotFound();
+            }
+            return View(medicamentos);
+        }
+
+        // POST: Medicamentos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MedicamentosId,Medicamento,Stock")] Medicamentos medicamentos)
+        {
+            if (id != medicamentos.MedicamentosId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(medicamentos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedicamentosExists(medicamentos.MedicamentosId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(medicamentos);
+        }
+
+        // GET: Medicamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicamentos = await _context.Medicamentos
+                .FirstOrDefaultAsync(m => m.MedicamentosId == id);
+            if (medicamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicamentos);
+        }
+
+        // POST: Medicamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var medicamentos = await _context.Medicamentos.FindAsync(id);
+            _context.Medicamentos.Remove(medicamentos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedicamentosExists(int id)
+        {
+            return _context.Medicamentos.Any(e => e.MedicamentosId == id);
+        }
+    }
+}
